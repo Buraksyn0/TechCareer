3bd29bc709fcc82eb53125abce67ba15f6a28dff
Burak Sayan <94619094+Buraksyn0@users.noreply.github.com>
2024-10-10 12:43:45 +0300

    Add files via upload

---
+++ degiskenlerOdev.dart
@@ -0,0 +1,99 @@
+void main() {
+  var ilceAdi = "Dalaman";
+  print("Türkiye'nin en güzel ilçelerinden biri $ilceAdi'dır");
+
+  var kita = "Avrupa";
+  print("Almanya $kita kıtasına ait bir ülkedir.");
+
+  int faks = 05454404838;
+  print("Telefon numaram:$faks");
+
+  int postaKodu = 48770;
+  print("Dalaman'ın posta kodu $postaKodu'dir.");
+
+  var instaAdresi = "buraksyn_";
+  print("İnstagram adresim:$instaAdresi");
+
+  var calistigimBolum = "Bilgisayar Mühendisliği";
+  print("Süleyman Demirel Üniversitesi'nde $calistigimBolum öğrencisiyim");
+
+  var urunMiktari = 5;
+
+  var musteriSoyadi = "SAYAN";
+  var odemeMiktari = 1000;
+  print(
+      "$musteriSoyadi soyisimli müşterimizin $odemeMiktari liralık son ödemesi kalmıştır.");
+
+  var dogumTarihi = 2001;
+  print("Doğum tarihim 19 Ekim 2001'dir.");
+
+  var borc = 1000;
+  print("$musteriSoyadi soyisimli müşterimizin $borc liralık borcu vardır.");
+
+  var medeniHal = "Bekar";
+  print(
+      "$ilceAdi ilçesinde yaşayan Burak $musteriSoyadi'ın medeni hali $medeniHal dır");
+
+  var videoYorumu;
+
+  var odemeSaati = "19.30";
+  print(
+      "Sayın $musteriSoyadi ödemenizi en geç saat $odemeSaati a kadar ödemenizi rica eder iyi günler dileriz.");
+
+  var eftMiktari;
+  var satilanMiktar = 15;
+  var telefonModeli = "Iphone 11";
+  String dergiAdi = "Bilim Çocuk";
+  String basimTarihi = "19/10/2001";
+  int zamMiktari = 3600;
+  int daireSayisi = 10;
+
+  String enlem = "36–42°";
+  String boylam = "26–45°";
+  print(
+      "Türkiye $enlem Kuzey enlemleri ve $boylam Doğu boylamları arasında yer alır. ");
+
+  String yemekAdi = "İskender";
+  int urunFiyat = 5500;
+
+  var sirket = "TechCareer";
+  print("$sirket'in bootcampi kendimi geliştirmek için güzel bir fırsat");
+
+  var videoAdi = "Burak";
+  double muzikSuresi = 3.35;
+  double mekanPuani = 4.5;
+  var dosyaAdi = "DartDersleri";
+  var resimFormati = "JPG.";
+
+  var renk = "Sarı";
+  var renk2 = "Lacivert";
+  print(
+      "Türkiye'nin en büyük futbol kulübü $renk ve $renk2 renklerine sahiptir.");
+
+  var renkKodu = "#ffff00";
+  var bilgisayarModeli = "Monster Abra A7";
+  var ekranBoyutu = "17,3 inç ";
+  var kullanimSuresi = "5 saat";
+
+  var basinc = 133.322;
+  print("1 Tor basınç $basinc paskala denktir.");
+
+  var etkinlikGunu = "Pazar";
+  print(
+      "Kulübümüzün düzenleyeceği tanışma kahvaltısı etkinliği $etkinlikGunu günü gerçekleşecektir.");
+
+  var odemeGunu = "Pazartesi";
+  print("Son ödeme gününüz $odemeGunu günüdür.İyi günler.");
+
+  var yolculukCikisTarihi = "10.10.2024";
+  var mahalleAdi = "Karaçalı Mahallesi";
+
+  var otobusHatti = 4;
+  print("Üniversiteye giden otobüs $otobusHatti numaralı otobüs hattıdır.");
+
+  var kullanilanDakika = 65;
+  var kargoTakipNo = 2121032035;
+  var kuponSuresi = "5 gün";
+  var kuponKodu = "10EKİM";
+  var faturaTarihi = "10.10.2024";
+}
